538bf8790924b850cfd3724f26953080a60892a6
dahn0209 <dahn0209@gmail.com>
2022-05-04 19:37:48 -0400

    fixed footer

---
+++ client/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {FooterStyled} from './Footer.styled'
+
+const Footer = () => {
+  return <FooterStyled>deee</FooterStyled>
+}
+
+export default Footer
---
+++ client/components/Footer.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const FooterStyled = styled.footer`
+  position: fixed;
+  padding: 10px 10px 0px 10px;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  color: white;
+  background: black;
+`
