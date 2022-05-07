9f0450ebef6a2f6f089279ec2b6620f508208e6f
dahn0209 <dahn0209@gmail.com>
2022-05-07 01:01:59 -0400

    changes

---
+++ client/components/AddPlan.js
@@ -0,0 +1,75 @@
+// import React from 'React'
+// import {createNewPlan} from '../store/plans'
+// import {connect} from 'react-redux'
+
+// const defaultState = {
+//   name: '',
+//   description: '',
+// }
+
+// class AddNewPlan extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = defaultState
+
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
+
+//   handleChange(event) {
+//     this.setState({
+//       [event.target.name]: event.target.value
+//     })
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault()
+//     this.props.createNewPlan({...this.state})
+//     this.setState(defaultState)
+//   }
+
+//   render() {
+//     const {name, description} = this.state
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <h1>New Plan Details</h1>
+
+//           <label htmlFor="name">Plan Name</label>
+//           <input
+//             type="text"
+//             name="name"
+//             value={name}
+//             placeholder="Plan Name"
+//             onChange={this.handleChange}
+//           />
+
+//           <label htmlFor="description">Plan Description</label>
+//           <input
+//             type="text"
+//             name="description"
+//             value={description}
+//             placeholder="Plan Description"
+//             onChange={this.handleChange}
+//           />
+
+//           <button type="submit">Add New Plan</button>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+
+// const mapState = state => {
+//   return {
+//     newPlan: state.plans
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     createNewPlan: plan => dispatch(createNewPlan(plan))
+//   }
+// }
+
+// export default connect(mapState, mapDispatch)(AddNewPlan)
--- client/components/Plans.js
+++ client/components/Plans.js
@@ -8,45 +8,46 @@ import {Link} from 'react-router-dom'
 // import AddNewPlan from './AddPlan'
 
 export class AllPlans extends React.Component {
-  componentDidMount() {
-    this.props.FetchProducts()
-  }
+  // componentDidMount() {
+  //   this.props.FetchProducts()
+  // }
 
   render() {
-    const plans = this.props.Plans
+    // const plans = this.props.Plans
     return (
-      <div>
-        {/* <AddNewPlan /> */}
-        <h1>All Plans</h1>
-        <h2>List of Plan:</h2>
-        {plans.map(plan => {
-          return (
-            <div className="all-product-list" key={plan.id}>
-              <div>
-                <Link to={`/products/${plan.id}`}>
-                  <h2>{plan.name}</h2>
-                  {/* <img src={product.imageUrl} /> */}
-                </Link>
-                <p>{plan.description}</p>
-                {/* <h3>Price:{product.price / 100}</h3> */}
-                <div>
-                  <Link to={`/plans/${plan.id}/edit`}>
-                    <button type="button">Edit Plan</button>
-                  </Link>
+      // <div>
+      //   {/* <AddNewPlan /> */}
+      //   <h1>All Plans</h1>
+      //   <h2>List of Plan:</h2>
+      //   {plans.map(plan => {
+      //     return (
+      //       <div className="all-product-list" key={plan.id}>
+      //         <div>
+      //           <Link to={`/products/${plan.id}`}>
+      //             <h2>{plan.name}</h2>
+      //             {/* <img src={product.imageUrl} /> */}
+      //           </Link>
+      //           <p>{plan.description}</p>
+      //           {/* <h3>Price:{product.price / 100}</h3> */}
+      //           <div>
+      //             <Link to={`/plans/${plan.id}/edit`}>
+      //               <button type="button">Edit Plan</button>
+      //             </Link>
 
-                  <button
-                    type="button"
-                    onClick={() => this.props.deletePlanThunk(plan)}
-                  >
-                    Remove Plan
-                  </button>
-                </div>
-              </div>
-            </div>
-          )
-        })}
-        <div />
-      </div>
+      //             <button
+      //               type="button"
+      //               onClick={() => this.props.deletePlanThunk(plan)}
+      //             >
+      //               Remove Plan
+      //             </button>
+      //           </div>
+      //         </div>
+      //       </div>
+      //     )
+      //   })}
+      //   <div />
+      // </div>
+      <div>HELLO!!!!</div>
     )
   }
 }
